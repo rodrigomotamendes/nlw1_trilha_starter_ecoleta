a3c25f3ce23135b5e80753eb26d745854d63d6f7
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2021-03-05 22:47:47 -0300

    Update create-point.html and .js

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -16,10 +16,26 @@ populateUFs()
 
 function getCities(event) {
   const citySelect = document.querySelector("select[name=city]")
+  const stateInput = document.querySelector("input[name=state]")
 
   const ufValue = event.target.value
 
+  const indexOfSelectedState = event.target.selectedIndex
+  stateInput.value = event.target.options[indexOfSelectedState].text
+
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+  fetch(url)
+    .then(res => res.json())
+    .then(cities => {
+
+      for (const city of cities) {
+        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      }
+
+      citySelect.disabled=false
+
+    })
 }
 
 document
