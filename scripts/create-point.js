0723cda79ca96ef976f8a50ae23a0739f8ae1dd8
Rodrigo Mota Mendes <66222801+rodrigomotamendes@users.noreply.github.com>
2021-03-09 13:14:46 -0300

    Update - Aula 03 - Escolha da stack

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -45,3 +45,40 @@ function getCities(event) {
 document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities)
+
+  const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+  for(const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+  }
+
+  const collectedItems = document.querySelector("input[name=items]")
+
+  let selectedItems = []
+
+  function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex(item => {
+      const itemFound = item == itemId
+      return itemFound
+    })
+
+    if(alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => {
+        const itemIsDifferent = item != itemId
+        return itemIsDifferent
+      })
+
+      selectedItems = filteredItems
+    } else {
+      selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems
+    
+  }
